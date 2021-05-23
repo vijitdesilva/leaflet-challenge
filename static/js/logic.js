efeae00545082001b97371b0126069a79dcda595
vijitdesilva <vijit.desilva@gmail.com>
2021-05-23 00:04:08 -0400

    updated

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,10 +1,10 @@
-/*Setting the map to center around the middle of the United States and attaching a zoom level of 4 so that the continental United States is showing */
+// Create the map with our layers
 const myMap = L.map("map", {
   center: [37.09, -95.71],
   zoom: 4
 });
 
-/* Using Leaflet's street map to as the background for our analysis */
+// Create the tile layer that will be the background of our map
 const streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
@@ -15,7 +15,7 @@ const streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{
 
 /* This allows us to call in the URL for earthquakes in the past seven days and set it to the global variable URL.  If we want to change the dimensions of the data
 that we are pulling in, this would be the place to change that */
-
+//   https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
 var URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 /* Bringing in the dataset and then placing markers with appropriate size and color related to the magnitude of the quake */
